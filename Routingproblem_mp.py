841356de824cc85dfd7975c71f73b8817b67545d
Sebastian Lauber <dev@slauber.de>
2018-01-19 23:12:05 +0100

    Nope, still en route

--- Routingproblem_mp.py
+++ Routingproblem_mp.py
@@ -32,11 +32,11 @@ AUFTRAG_BRAUCHT_SKILL = np.array(
 )
 
 AUFTRAGSDAUER = np.array([45, 10, 10])
-FRUESTER_START = np.array([200, 0, 0])
+FRUESTER_START = np.array([0, 0, 0])
 SPAETESTER_START = np.array([300, 400, 0])
 
-H = 400
-H_max = 500
+H = 300
+H_max = 600
 
 STRAFE_AUFTRAG = (100, 100, 100)
 STRAFE_TECHNIKER = (0.3, 10.5)
@@ -73,7 +73,7 @@ start_zeit = mdl.integer_var_list(ANZ_WEGPUNKTE, name="Startzeit")
 #
 # Ein Auftrag muss vor H-Max enden & Techniker muss spätestens bis H_max zuhause sein
 mdl.add_constraints(
-    [FRUESTER_START[i] + AUFTRAGSDAUER[i] <= start_zeit[i] + AUFTRAGSDAUER[i] for i in range(ANZ_AUFTRAEGE)]
+    [FRUESTER_START[i] <= start_zeit[i] for i in range(ANZ_AUFTRAEGE)]
 )
 
 mdl.add_constraints(
@@ -83,10 +83,11 @@ mdl.add_constraints(
 mdl.add_equivalence_constraints(
     [
         mdl.add_equivalence(x[(m, i, m + ANZ_AUFTRAEGE)],
-                            start_zeit[i] + AUFTRAGSDAUER[i] + DISTANZMATRIX[i][m] <= H_max)
+                            start_zeit[i] + AUFTRAGSDAUER[i] + DISTANZMATRIX[i][m + ANZ_AUFTRAEGE] <= H_max)
         for i in range(ANZ_AUFTRAEGE) for m in range(ANZ_TECHNIKER)
     ]
 )
+
 
 mdl.add_constraints(
     [
@@ -140,6 +141,15 @@ for m in range(ANZ_TECHNIKER):
     for j in range(ANZ_AUFTRAEGE):
         _temp.append(x[(m, j, m + ANZ_AUFTRAEGE)])
     mdl.add_constraint(mdl.sum(_temp) <= 1)
+
+mdl.add_constraints(
+    x[(m, i, j)] <= mdl.sum(
+        x[(m, m + ANZ_AUFTRAEGE, t)] for t in range(ANZ_AUFTRAEGE)
+    )
+    for m in range(ANZ_TECHNIKER)
+    for i in range(ANZ_WEGPUNKTE)
+    for j in range(ANZ_AUFTRAEGE)
+)
 
 # sum (t in Auftrag) x[m][m][t] == sum (t in Auftrag) x[m][t][m]
 for m in range(ANZ_TECHNIKER):
@@ -197,6 +207,21 @@ mdl.add_constraints(
     ]
 )
 
+# mdl.add_equivalence_constraints(
+#    mdl.add_equivalence(
+#        mdl.sum(
+#            x[(m,i,j)]
+#            for i in range(ANZ_WEGPUNKTE)
+#            for j in range(ANZ_AUFTRAEGE)
+#        ) >= 1,
+#        mdl.sum(
+#            x[(m,m,j)]
+#            for j in range(ANZ_AUFTRAEGE)
+#        ) == 1
+#    )
+#    for m in range(ANZ_TECHNIKER)
+# )
+
 mdl.add_constraints(
     [
         x[(m, i, i)] == 0
@@ -205,22 +230,34 @@ mdl.add_constraints(
     ]
 )
 
+mdl.add_constraints(
+    start_zeit[m + ANZ_AUFTRAEGE] == 0
+    for m in range(ANZ_TECHNIKER)
+)
+
+mdl.add_equivalence_constraints(
+    mdl.add_equivalence(
+        x[(m, i, j)],
+        start_zeit[j] >= (start_zeit[i] + AUFTRAGSDAUER[i] + DISTANZMATRIX[i][j])
+    )
+    for m in range(ANZ_TECHNIKER)
+    for i in range(ANZ_AUFTRAEGE)
+    for j in range(ANZ_WEGPUNKTE)
+    if i != j and i != m
+)
+
 # Entscheidungsausdrücke
 strafkosten_auftrag = mdl.sum(
-    [
-        mdl.max(0, start_zeit[i] + AUFTRAGSDAUER[i] - SPAETESTER_START[i]) * STRAFE_AUFTRAG[i]
-        for i in range(ANZ_AUFTRAEGE)
-    ]
+    mdl.max(0, start_zeit[i] + AUFTRAGSDAUER[i] - SPAETESTER_START[i]) * STRAFE_AUFTRAG[i]
+    for i in range(ANZ_AUFTRAEGE)
 )
 mdl.add_kpi(strafkosten_auftrag, "Strafkosten Auftrag")
 
 strafkosten_techniker = mdl.sum(
-    [
-        mdl.max(0, start_zeit[i] + AUFTRAGSDAUER[i], DISTANZMATRIX[i][m + ANZ_AUFTRAEGE] - H)
-        * STRAFE_TECHNIKER[m] * x[(m, i, m + ANZ_AUFTRAEGE)]
-        for i in range(ANZ_AUFTRAEGE)
-        for m in range(ANZ_TECHNIKER)
-    ]
+    mdl.max(0, start_zeit[i] + AUFTRAGSDAUER[i], DISTANZMATRIX[i][m + ANZ_AUFTRAEGE] - H)
+    * STRAFE_TECHNIKER[m] * x[(m, i, m + ANZ_AUFTRAEGE)]
+    for i in range(ANZ_AUFTRAEGE)
+    for m in range(ANZ_TECHNIKER)
 )
 mdl.add_kpi(strafkosten_techniker, "Strafkosten Techniker")
 
@@ -239,9 +276,9 @@ kosten = GEWICHT_STRAFE_AUFTRAG * strafkosten_auftrag + \
          GEWICHT_STRAFE_TECHNIKER * strafkosten_techniker + \
          GEWICHT_TRANSPORT_KOSTEN * transportkosten
 
+mdl.minimize(mdl.sum(start_zeit[i] for i in range(ANZ_AUFTRAEGE)))
+#mdl.minimize(kosten)
 
-mdl.print_information()
-mdl.minimize(kosten)
 print("\n", mdl.get_objective_expr())
 print(mdl.get_objective_sense(), "\n")
 #
