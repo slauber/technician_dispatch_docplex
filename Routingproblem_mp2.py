9bb20ce76291cec4c947328c80f52b0c6dc22b7c
Sebastian Lauber <dev@slauber.de>
2018-01-21 17:04:15 +0100

    First working version

--- Routingproblem_mp2.py
+++ Routingproblem_mp2.py
@@ -4,36 +4,19 @@ from docplex.mp.solution import SolveSolution
 
 
 class RoutingProblem:
-    TECHNIKER_HAT_SKILL = np.array(
-        [
-            [0, 1],
-            [1, 0]
-        ]
-    )
+    DISTANZMATRIX: np.array
+    TECHNIKER_HAT_SKILL: np.array
+    AUFTRAG_BRAUCHT_SKILL: np.array
 
-    DISTANZMATRIX = np.array(
-        [[0, 30, 70, 20, 15],
-         [30, 0, 45, 30, 25],
-         [70, 45, 0, 50, 45],
-         [20, 30, 50, 0, 30],
-         [15, 25, 45, 30, 0]]
-    )
+    AUFTRAGSDAUER: np.array
+    FRUESTER_START: np.array
+    SPAETESTES_ENDE: np.array
 
-    AUFTRAG_BRAUCHT_SKILL = np.array(
-        [[1, 0],
-         [1, 0],
-         [0, 1]]
-    )
+    H: int
+    H_max: int
 
-    AUFTRAGSDAUER = np.array([45, 10, 10])
-    FRUESTER_START = np.array([0, 0, 0])
-    SPAETESTES_ENDE = np.array([300, 400, 0])
-
-    H = 400
-    H_max = 500
-
-    STRAFE_AUFTRAG = (100, 100, 100)
-    STRAFE_TECHNIKER = (10.5, 10.5)
+    STRAFE_AUFTRAG: np.array
+    STRAFE_TECHNIKER: np.array
 
     KRAFTSTOFF_KOSTEN = 0.15
 
@@ -41,10 +24,10 @@ class RoutingProblem:
     GEWICHT_STRAFE_TECHNIKER = 100
     GEWICHT_TRANSPORT_KOSTEN = 1
 
-    ANZ_TECHNIKER = len(TECHNIKER_HAT_SKILL)
-    ANZ_AUFTRAEGE = len(AUFTRAG_BRAUCHT_SKILL)
-    ANZ_WEGPUNKTE = len(DISTANZMATRIX)
-    ANZ_SKILLS = len(TECHNIKER_HAT_SKILL[0])
+    ANZ_TECHNIKER: int
+    ANZ_AUFTRAEGE: int
+    ANZ_WEGPUNKTE: int
+    ANZ_SKILLS: int
 
     mdl: Model
 
@@ -52,19 +35,25 @@ class RoutingProblem:
                       tageslaenge: int, max_tageslaenge: int, seed: int):
         anz_wegpunkte = anz_techniker + anz_auftraege
         np.random.seed(seed)
-        distanzmatrix = np.random.randint(0, 150, size=(anz_wegpunkte, anz_wegpunkte))
+        distanzmatrix = np.random.randint(0, 75, size=(anz_wegpunkte, anz_wegpunkte))
         distanzmatrix = (distanzmatrix + distanzmatrix.T)
         np.fill_diagonal(distanzmatrix, 0)
+
+        self.H = tageslaenge
+        self.H_max = max_tageslaenge
 
         self.DISTANZMATRIX = distanzmatrix
         self.TECHNIKER_HAT_SKILL = np.random.randint(0, 2, size=(anz_techniker, anz_skills))
         self.AUFTRAG_BRAUCHT_SKILL = np.random.randint(0, 2, size=(anz_auftraege, anz_skills))
-        self.AUFTRAGSDAUER = np.random.randint(10, 180, size=anz_auftraege)
-        self.FRUESTER_START = np.random.randint(0, 250, size=anz_auftraege)
+        self.AUFTRAGSDAUER = np.random.randint(10, 120, size=anz_wegpunkte)
+        for i in range(anz_techniker):
+            self.AUFTRAGSDAUER[anz_auftraege + i] = 0
+
+        self.FRUESTER_START = np.random.randint(0, 10, size=anz_auftraege)
 
         self.SPAETESTES_ENDE = np.zeros(anz_auftraege)
         for i in range(anz_auftraege):
-            self.SPAETESTES_ENDE[i] = self.FRUESTER_START[i] + self.AUFTRAGSDAUER[i] + np.random.randint(0, 120)
+            self.SPAETESTES_ENDE[i] = self.FRUESTER_START[i] + self.AUFTRAGSDAUER[i] + np.random.randint(60, 180)
 
         self.STRAFE_AUFTRAG = np.random.randint(50, 500, size=anz_auftraege)
         self.STRAFE_TECHNIKER = np.random.randint(5, 20, size=anz_techniker)
@@ -84,8 +73,14 @@ class RoutingProblem:
             print("Warning - Generated data was inconsistent - Regenerating using seed ", seed + 1)
             self.generate_data(anz_techniker, anz_auftraege, anz_skills, tageslaenge, max_tageslaenge, seed + 1)
 
+    def set_data(self):
+        pass
+
     def print_input(self):
         print('Distanzmatrix\n', self.DISTANZMATRIX)
+        print('Frühster Start\n', self.FRUESTER_START)
+        print('Auftragsdauer\n', self.AUFTRAGSDAUER)
+        print('Spätestes Ende\n', self.SPAETESTES_ENDE)
         print('Technikerskills\n', self.TECHNIKER_HAT_SKILL)
         print('Auftragskills\n', self.AUFTRAG_BRAUCHT_SKILL)
 
@@ -101,60 +96,88 @@ class RoutingProblem:
         x = mdl.binary_var_cube(self.ANZ_TECHNIKER, self.ANZ_WEGPUNKTE, self.ANZ_WEGPUNKTE, name="Fahrten")
         start_zeit = mdl.integer_var_list(self.ANZ_WEGPUNKTE, name="Startzeit")
 
-        mdl.maximize(self.H_max - start_zeit[0] + self.H_max - start_zeit[1] + self.H_max - start_zeit[2])
+        # Entscheidungsausdrücke
+        strafkosten_auftrag = mdl.sum(
+            mdl.max(0, start_zeit[i] + self.AUFTRAGSDAUER[i] - self.SPAETESTES_ENDE[i]) * self.STRAFE_AUFTRAG[i]
+            for i in r_auftraege
+        )
+        mdl.add_kpi(strafkosten_auftrag, "Strafkosten Auftrag")
+        #
+        strafkosten_techniker = mdl.sum(
+            mdl.max(0, start_zeit[i] + self.AUFTRAGSDAUER[i], self.DISTANZMATRIX[i][m + self.ANZ_AUFTRAEGE] - self.H)
+            * self.STRAFE_TECHNIKER[m] * x[(m, i, m + self.ANZ_AUFTRAEGE)]
+            for i in r_auftraege
+            for m in r_techniker
+        )
+        mdl.add_kpi(strafkosten_techniker, "Strafkosten Techniker")
+        #
+        transportkosten = mdl.sum(
+            x[(m, i, j)] * self.DISTANZMATRIX[i][j] * self.KRAFTSTOFF_KOSTEN
+            for m in r_techniker
+            for i in r_wegpunkte
+            for j in r_wegpunkte
+        )
+        mdl.add_kpi(transportkosten, "Transportkosten")
+
+        mdl.minimize(
+            strafkosten_auftrag * self.GEWICHT_STRAFE_AUFTRAG + strafkosten_techniker * self.GEWICHT_STRAFE_TECHNIKER
+        )
+
+        # mdl.minimize(mdl.sum(
+        #    start_zeit[i]
+        #    for i in r_auftraege
+        # ))
+
+
         print("\n", mdl.get_objective_expr())
         print(mdl.get_objective_sense(), "\n")
 
         # Startzeit eines Auftrags muss nach frühestem Startpunkt liegen
         mdl.add_constraints(
-            [self.FRUESTER_START[i] <= start_zeit[i] for i in r_auftraege]
+            self.FRUESTER_START[i] <= start_zeit[i]
+            for i in r_auftraege
         )
 
         # Startzeit und Auftragsdauer müssen vor H_max enden
         mdl.add_constraints(
-            [start_zeit[i] + self.AUFTRAGSDAUER[i] <= self.H_max for i in r_auftraege]
+            start_zeit[i] + self.AUFTRAGSDAUER[i] <= self.H_max
+            for i in r_auftraege
         )
 
-        mdl.add_equivalence_constraints(
-            [
-                mdl.add_equivalence(x[(m, i, m + self.ANZ_AUFTRAEGE)],
-                                    start_zeit[i] + self.AUFTRAGSDAUER[i] + self.DISTANZMATRIX[i][
-                                        m + self.ANZ_AUFTRAEGE] <= self.H_max)
-                for i in r_auftraege for m in r_techniker
-            ]
-        )
+        [
+            mdl.add_if_then(
+                x[(m, i, m + self.ANZ_AUFTRAEGE)] == 1,
+                start_zeit[i] + self.AUFTRAGSDAUER[i] + self.DISTANZMATRIX[i][
+                    m + self.ANZ_AUFTRAEGE] <= self.H_max
+            )
+            for i in r_auftraege for m in r_techniker
+        ]
 
         # Fährt nicht von anderem Wegpunkt zu fremden Depot
         mdl.add_constraints(
-            [
-                x[(m, i, t + self.ANZ_AUFTRAEGE)] == 0
-                for m in r_techniker
-                for i in r_wegpunkte
-                for t in r_techniker
-                if (t != m)
-            ],
+            x[(m, i, t + self.ANZ_AUFTRAEGE)] == 0
+            for m in r_techniker
+            for i in r_wegpunkte
+            for t in r_techniker
+            if (t != m)
         )
 
         # Fährt nicht von anderem Depot zu einem Wegpunkt
         mdl.add_constraints(
-            [
-                x[(m, i + self.ANZ_AUFTRAEGE, j)] == 0
-                for m in r_techniker
-                for i in r_techniker
-                for j in r_wegpunkte
-                if m != i
-            ]
+            x[(m, i + self.ANZ_AUFTRAEGE, j)] == 0
+            for m in r_techniker
+            for i in r_techniker
+            for j in r_wegpunkte
+            if m != i
         )
 
         # Fährt maximal einmal von seinem Depot los
         mdl.add_constraints(
-            [
-                mdl.sum(
-                    x[(m, m + self.ANZ_AUFTRAEGE, j)]
-                    for j in r_auftraege
-                ) <= 1
-                for m in r_techniker
-            ]
+            mdl.sum(
+                x[(m, m + self.ANZ_AUFTRAEGE, j)]
+                for j in r_auftraege
+            ) <= 1
+            for m in r_techniker
         )
 
         # Fährt maximal einmal nach Hause
@@ -203,55 +226,36 @@ class RoutingProblem:
 
         # Wenn er von einem Auftrag wegfährt, dann muss er dort auch hingefahren sein
         mdl.add_constraints(
-            [
-                x[(m, j, l)] <= mdl.sum([
-                    x[(m, t, j)]
-                    for t in r_wegpunkte
-                    if j != t
-                    if l != t
-                ]
-                )
-                for m in r_techniker
-                for j in r_auftraege
-                for l in r_wegpunkte
-                if l != j
+            x[(m, j, l)] <= mdl.sum([
+                x[(m, t, j)]
+                for t in r_wegpunkte
+                if j != t
+                if l != t
             ]
+            )
+            for m in r_techniker
+            for j in r_auftraege
+            for l in r_wegpunkte
+            if l != j
         )
 
-        # # Wenn er einen Auftrag besucht, dann muss er von dort auch wieder wegfahren
-        # mdl.add_constraints(
-        #     x[(m, l, j)] <= mdl.sum(
-        #         x[(m, j, i)]
-        #         for i in range(ANZ_WEGPUNKTE)
-        #         if i != j
-        #     )
-        #     for m in range(ANZ_TECHNIKER)
-        #     for l in range(ANZ_WEGPUNKTE)
-        #     for j in range(ANZ_AUFTRAEGE)
-        #     if l != j
-        # )
-
-        # Jeder Auftrag muss besucht worden sein
+        # Jeder Auftrag muss genaz einmal besucht worden sein
         mdl.add_constraints(
-            [
-                mdl.sum(
-                    [
-                        x[(m, j, i)]
-                        for m in r_techniker
-                        for j in r_wegpunkte
-                    ]
-                ) == 1
-                for i in r_auftraege
-            ]
+            mdl.sum(
+                [
+                    x[(m, j, i)]
+                    for m in r_techniker
+                    for j in r_wegpunkte
+                ]
+            ) == 1
+            for i in r_auftraege
         )
 
         # Ein Auftrag besucht sich nicht selbst
         mdl.add_constraints(
-            [
-                x[(m, i, i)] == 0
-                for m in r_techniker
-                for i in r_auftraege
-            ]
+            x[(m, i, i)] == 0
+            for m in r_techniker
+            for i in r_auftraege
         )
 
         # Defaultwert für Technikerstart
@@ -259,6 +263,18 @@ class RoutingProblem:
             start_zeit[m + self.ANZ_AUFTRAEGE] == 0
             for m in r_techniker
         )
+
+        # Zeitconstraints, Startzeiten müssen der Route entsprechen
+        [
+            mdl.add_if_then(
+                x[(m, i, j)] == 1,
+                start_zeit[j] >= (start_zeit[i] + self.AUFTRAGSDAUER[i] + self.DISTANZMATRIX[i][j])
+            )
+            for m in r_techniker
+            for i in r_wegpunkte
+            for j in r_auftraege
+            if i != j
+        ]
 
         # Skill constraint
         mdl.add_constraints(
@@ -269,10 +285,10 @@ class RoutingProblem:
             for s in r_skills
         )
 
-        print(mdl.export_as_lp_string())
+        #print(mdl.export_as_lp_string())
         self.mdl = mdl
 
-    def solve_model(self):
+    def solve_model(self, timeout: int = 10):
         self.mdl.solve()
         self.mdl.report()
         print(self.mdl.get_solve_details())
@@ -284,8 +300,8 @@ class RoutingProblem:
 if __name__ == '__main__':
     problem = RoutingProblem()
 
-    problem.generate_data(anz_techniker=4, anz_auftraege=3, anz_skills=5, tageslaenge=500, max_tageslaenge=600,
-                          seed=10000)
+    problem.generate_data(anz_techniker=3, anz_auftraege=5, anz_skills=2, tageslaenge=500, max_tageslaenge=600,
+                          seed=10)
     problem.print_input()
 
     problem.create_model()
